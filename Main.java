9b95fb280f93b2243df3e03c60880b9961259421
andrew-erasmus <andrew.r.eras@gmail.com>
2023-04-03 20:16:11 +0200

    Add the fixed Main.java, Post.java, User.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+//import TokTik-app.User;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        User myUser = new User("","");
+        System.out.println(myUser);
+        
+    }
+}
