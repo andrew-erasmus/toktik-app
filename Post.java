c4830444e132616b06103cf112207cc3ea87db1a
andrew-erasmus <andrew.r.eras@gmail.com>
2023-04-05 18:16:02 +0200

    Add the Post object and functionality

--- Post.java
+++ Post.java
@@ -1,3 +1,28 @@
 public class Post {
+    private String title;
+    private String fileName;
+    private int numLikes;
     
+    public Post(String inTitle, String inFileName, int inNumLikes){
+        title = inTitle;
+        fileName = inFileName;
+        numLikes=inNumLikes;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getFName(){
+        return fileName;
+    }
+
+    public int getLikes(){
+        return numLikes;
+    }
+
+    public String toString(){
+        String output="Title: "+title+"\nFile Name: "+fileName+"\nLikes: "+numLikes;
+        return output;
+    }
 }
